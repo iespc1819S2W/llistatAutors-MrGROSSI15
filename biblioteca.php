76213f060c186eebaa277896a880278a2e178ccb
MrGROSSI15 <marcgrossi@iespaucasesnoves.cat>
2018-11-21 20:58:46 +0100

    ordenacio id,nom i casi cerca

--- biblioteca.php
+++ biblioteca.php
@@ -9,15 +9,29 @@ if (mysqli_connect_errno()) {
         printf("Error: %s\n", mysqli_connect_error()); exit();
      }
      $orderby = "ID_AUT ASC";
-
+     $autor="";
+     
      if(isset($_POST['ID_AUT_ASC'])){
-         $orderby = "ID_AUT ASC";
-     }
-
-     if(isset($_POST['ID_AUT_DESC'])){
+       $orderby = "ID_AUT ASC";
+      }
+      
+      if(isset($_POST['ID_AUT_DESC'])){
         $orderby = "ID_AUT DESC";
-    }
+      }
 
+      if(isset($_POST['NOM_AUT_ASC'])){
+        $orderby = "NOM_AUT ASC";
+      }
+      if(isset($_POST['NOM_AUT_DESC'])){
+        $orderby = "NOM_AUT DESC";
+      }
+
+      if(isset($_POST['cercar'])){
+        $autor = $_POST['cerca'];
+        $query = "SELECT * FROM autors WHERE NOM_AUT LIKE '%" .$autor. "%'"; //ID_AUT = $valor or
+      }else{
+        $query = "SELECT * FROM autors ORDER BY $orderby";
+      }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,12 +45,18 @@ if (mysqli_connect_errno()) {
     <title>Document</title>
 </head>
 <body>
+
 <form action="" method="POST">
 <div class="container">
   <h2>BIBLIOTECA</h2>
 <button name="ID_AUT_ASC" class="btn btn-dark">CODI ASCENDENT</button>
 <button name="ID_AUT_DESC" class="btn btn-dark">CODI DESCENDENT</button>
-       
+<button name="NOM_AUT_ASC" class="btn btn-dark">NOM ASCENDENT</button>
+<button name="NOM_AUT_DESC" class="btn btn-dark">NOM DESCENDENT</button>
+<form action="/action_page.php">
+      <input type="text" placeholder="Cerca.." name="cerca" id="cerca">
+      <button class="btn btn-dark" id="cercar" >CERCA</button>
+    </form>
   <table class="table table-dark table-striped">
     <thead>
       <tr>
@@ -46,9 +66,8 @@ if (mysqli_connect_errno()) {
     </thead>
     <tbody>
 <?php 
-$query =("SELECT * FROM `autors` ORDER BY $orderby limit 10");
+// $query =("SELECT * FROM autors ORDER BY $orderby limit 10 ");
 if ($result = $mysqli->query($query)) {
-
     while ($row = $result->fetch_assoc()) {
         echo ("<tr>");
         echo ("<th scope='row'> ". $row["ID_AUT"]."</th>");
@@ -56,6 +75,8 @@ if ($result = $mysqli->query($query)) {
         echo ("</tr>");
     }
 }
+
+
 ?>
     </tbody>
   </table>
