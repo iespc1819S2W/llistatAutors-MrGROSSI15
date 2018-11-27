805b5cd40fa512155768d250b7918ff5cf42e45e
MrGROSSI15 <marcgrossi@iespaucasesnoves.cat>
2018-11-27 20:57:17 +0100

    PAGINACIO FETA, pero la darrera pàgina se passa

--- biblioteca.php
+++ biblioteca.php
@@ -9,13 +9,24 @@ if (mysqli_connect_errno()) {
         printf("Error: %s\n", mysqli_connect_error()); exit();
       }
       $orderby = "ID_AUT ASC";
-      $autor="";
-      $codi="";
-      $limits = "limit 20";
-      $row=['ID_AUT'];
-      // $query = "SELECT * FROM autors ORDER BY $orderby $limits";
-      // $lines = mysql_num_rows($query);
-     
+      If(isset($_POST['orderby'])){
+        $orderby = $_POST['orderby'];
+      }
+      $cerca="";
+      if(isset($_POST['cerca'])){
+        $cerca = $_POST['cerca'];
+      }
+      
+      $limits = 20;
+      $pagina= 0;
+      if(isset($_POST['pagina'])){
+        $pagina = $_POST['pagina'];
+      }
+      $result= $mysqli->query("SELECT * FROM autors WHERE NOM_AUT LIKE '%" .$cerca. "%' OR ID_AUT LIKE '%" .$cerca. "%'");
+    
+      $numRegistresPag= mysqli_num_rows($result);
+      $numPag= ceil($numRegistresPag/$limits);
+
      if(isset($_POST['ID_AUT_ASC'])){
        $orderby = "ID_AUT ASC";
       }
@@ -30,28 +41,32 @@ if (mysqli_connect_errno()) {
       if(isset($_POST['NOM_AUT_DESC'])){
         $orderby = "NOM_AUT DESC";
       }
-
-      if(isset($_POST['butcercar'])){
-        $autor = $_POST['cerca'];
-        $query = "SELECT * FROM autors WHERE NOM_AUT LIKE '%" .$autor. "%' $limits"; //ID_AUT = $valor or
-      }else{
-        $query = "SELECT * FROM autors ORDER BY $orderby $limits";
-      }
-      if(isset($_POST['butcercarC'])){
-        $codi = $_POST['cercaC'];
-        $query = "SELECT * FROM autors WHERE ID_AUT LIKE '%" .$codi. "%' $limits"; //ID_AUT = $valor or
-      }else{
-        $query = "SELECT * FROM autors ORDER BY $orderby $limits";
-      }
-
+      
+      
       if(isset($_POST['PRIMER'])){
-        $query = "SELECT * FROM autors ORDER BY $orderby $limits";
-       
+        $pagina=0;
+        
       }
+      
       if(isset($_POST['SEGUENT'])){
-        $query = "SELECT * FROM autors ORDER BY $orderby $limits";
+        if($pagina < $numPag){
+          $pagina = $pagina +1;
+        }
       }
-?>
+      if(isset($_POST['ANTERIOR'])){
+        if($pagina > 0){
+          $pagina = $pagina -1;
+        }
+      }
+      if(isset($_POST['DARRERA'])){
+        
+          $pagina = $numPag;
+        
+      }
+      
+      $tuplaInici=$pagina * $limits;
+      $query = "SELECT * FROM autors WHERE NOM_AUT LIKE '%" .$cerca. "%' OR ID_AUT LIKE '%" .$cerca. "%' ORDER BY $orderby LIMIT $tuplaInici,$limits"; 
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -73,13 +88,15 @@ if (mysqli_connect_errno()) {
 <button name="NOM_AUT_ASC" class="btn btn-dark">NOM ASCENDENT</button>
 <button name="NOM_AUT_DESC" class="btn btn-dark">NOM DESCENDENT</button>
 <button name="PRIMER" class="btn btn-dark">PRIMER</button>
-<button name="DARRER" class="btn btn-dark">DARRER</button>
+<button name="DARRERA" class="btn btn-dark">DARRERA</button>
 <button name="SEGUENT" class="btn btn-dark">SEGÜENT</button>
 <button name="ANTERIOR" class="btn btn-dark">ANTERIOR</button>
-      <input type="text" placeholder="Cerca NOM..." name="cerca" id="cerca">
-      <button class="btn btn-dark" id="butcercar" name="butcercar">CERCA PER NOM</button>
-      <input type="text" placeholder="Cerca CODI..." name="cercaC" id="cercaC">
-      <button class="btn btn-dark" id="butcercarC" name="butcercarC">CERCA PER ID</button>
+  <input type="hidden" value="<?=$pagina?>" name="pagina" id="pagina">
+  <input type="hidden" value="<?=$orderby?>" name="orderby" id="orderby">
+      <input type="text" placeholder="Cerca NOM..." name="cerca" id="cerca" value="<?=$cerca?>">
+      <button class="btn btn-dark" id="butcercar" name="butcercar">CERCA PER NOM i ID</button>
+      
+      
     </form>
   <table class="table table-dark table-striped">
     <thead>
@@ -91,6 +108,7 @@ if (mysqli_connect_errno()) {
     <tbody>
 <?php 
 // $query =("SELECT * FROM autors ORDER BY $orderby limit 10 ");
+echo($query);
 if ($result = $mysqli->query($query)) {
     while ($row = $result->fetch_assoc()) {
         echo ("<tr>");
